e8eb3783ffeb4bbc148723ee38fd967d86c69100
Regina <reina5425@gmail.com>
2022-07-06 21:38:39 +0300

    feat: new features in index.js - new functions

--- js/index.js
+++ js/index.js
@@ -1,5 +1,32 @@
-function getName () {
-	let userName = prompt('Как тебя зовут?', 'Имя');
+// Function Declaration
+// function getName () {
+// 	let userName = prompt('Как тебя зовут?', 'Имя');
 
-	alert(`Привет, ${userName}`);
+// 	alert(`Привет, ${userName}`);
+// }
+
+// стрелочная функция
+// const getName = () => {
+// 	let userName = prompt('Как тебя зовут?', 'Имя'); // input ввод
+
+// 	alert(`Привет, ${userName}`); //div вывод
+// };
+
+const helloInput = document.querySelector('.hello__input');
+
+function btnClick() {
+	helloInput.style.display = 'block';
+}
+
+const input = document.querySelector('.input');
+const outputUserName = document.querySelector('.output');
+
+
+function getUserName() {
+	const userName = input.value;
+	outputUserName.textContent = `Твое имя: ${userName}`;
+}
+
+function changeBackgroundColor () {
+	outputUserName.style.backgroundColor = '#fff';
 }
